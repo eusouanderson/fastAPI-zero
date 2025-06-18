05a35b7af915b5544c607426c8d2517aca904022
Anderson Rodrigues <eusouanderson@outlook.com>
2025-06-18 14:20:24 -0300

    Fundamentos do desenvolvimento web

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='API')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+async def home():
+    return """
+    <html>
+      <head><title>FastApi do Zero</title></head>
+      <body>
+        <h1>Olá, Mundo!</h1>
+        <p>Este é um HTML direto no FastAPI.</p>
+      </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,18 @@ from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
 
+client = TestClient(app)
+
 
 def test_root_deve_retornar_ola_mundo():
-    client = TestClient(app)
     response = client.get('/')
-
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_html_deve_retornar_um_html():
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'].startswith('text/html')
+    assert '<html>' in response.text
+    assert 'Olá, Mundo!' in response.text
